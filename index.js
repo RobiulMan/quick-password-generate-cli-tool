0f42cbfc79f986d95a1f35fa35a1e612435eee8b
Robul Islam <49164744+RobiulMan@users.noreply.github.com>
2021-07-06 02:45:22 +0600

    remove a line of code

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ program.version('1.0.0').description('quick password generator cli tool');
 program
   .option('-l, --length <number>', 'length of password', '8')
   .option('-s, --save ', 'save password to password.txt')
-  .parse()
   .option('-nn, --no-number ', 'remove numbers')
   .option('-ns, --no-symbols ', 'remove symbols')
   .parse();
